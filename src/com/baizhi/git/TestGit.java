6a7d0126323484e7b5dd90a78d57a4f8584a3436
mzlt1994 <mzlt940817@126.com>
2018-12-15 15:35:06 +0800

    第一次提交

---
+++ src/com/baizhi/git/TestGit.java
@@ -0,0 +1,34 @@
+package com.baizhi.git;
+
+public class TestGit {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestGit{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
